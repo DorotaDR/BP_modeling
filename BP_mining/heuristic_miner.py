51ec4e56a898957ec33fda3118bf13d6b696bbea
Dorota Dydo-Rożniecka <34707786+DorotaDR@users.noreply.github.com>
2020-04-24 17:04:33 +0200

    Add more heuristic miner :)

--- BP_mining/heuristic_miner.py
+++ BP_mining/heuristic_miner.py
@@ -18,21 +18,126 @@ class HeuristicMiner(BaseMiner):
         self.TI = self.log.get_first_tasks()
         self.TO = self.log.get_last_tasks()
 
-        self.relation_frequency_df = pd.DataFrame()
+        self.relation_frequency_df: pd.DataFrame = None
+        self.rel_2loop_frequency_df: pd.DataFrame = None
+        self.norm_relation_frequency_df: pd.DataFrame = None
+        self.norm_rel_2loop_frequency_df: pd.DataFrame = None
 
         _relations = dict()
         for task in self.TL:
             _relations[task] = dict()
-        self._dict_relations_frequency = OrderedDict(sorted(_relations.items()))
+        self._dict_relations_frequency = OrderedDict(sorted(_relations.copy().items()))
+
+        _relations = dict()
+        for task in self.TL:
+            _relations[task] = dict()
+        self._dict_rel_2loop_frequency = OrderedDict(sorted(_relations.copy().items()))
 
         self._direct_successor_pairs: list = None
 
 
     def execute_algorithm(self):
+
+        self.set_relation_frequency_matrix()
+        self.set_2loop_frequency_matrix()
+
+        self.normalize_relation_frequency_matrix()
+        self.normalize_2loop_frequency_matrix()
+
+        print(f"Relation matrix \n {self.get_relation_frequency_matrix()}\n")
+
+        print(f"Loop  matrix \n {self.get_2loop_frequency_matrix()}\n")
+
+    def get_relation_frequency_matrix(self, normalized: bool = True):
+        return self.norm_relation_frequency_df if normalized else self.relation_frequency_df
+
+    def get_2loop_frequency_matrix(self, normalized: bool = True):
+        freq_df = self.norm_rel_2loop_frequency_df.copy(deep=True) if normalized \
+            else self.rel_2loop_frequency_df.copy(deep=True)
+        freq_df.index = [f"{i}_{i}" for i in freq_df.index]
+        return freq_df
+
+    def set_relation_frequency_matrix(self):
+        if not self._direct_successor_pairs:
+            self._set_direct_successors()
+
+        for pair in set(self._direct_successor_pairs):
+            self._dict_relations_frequency[pair[1]][pair[0]] =self._direct_successor_pairs.count(pair)
+
+        self.relation_frequency_df = pd.DataFrame.from_dict(self._dict_relations_frequency)
+        self.relation_frequency_df = self._fill_freq_matrix_with_nan(self.relation_frequency_df)
+
+    def set_2loop_frequency_matrix(self):
+
+        # find 2-loop patterns in logs:
+        two_loop_list = []
+        for case in self.log.log:
+            tasks_names_list = [task['concept:name'] for task in case]
+            for i in range(0, len(tasks_names_list) - 3):
+                # miss patterns as: "aaa" (all the same)
+                if tasks_names_list[i] == tasks_names_list[i + 2] and tasks_names_list[i] != tasks_names_list[i+1]:
+                    two_loop_list.append(tuple(tasks_names_list[i:i + 3]))
+
+        #count frequencies of each 2-loop pattern and safe
+        for (a_1, b, a_2) in set(two_loop_list):
+            self._dict_rel_2loop_frequency[b][a_1] = two_loop_list.count((a_1, b, a_2))
+
+        self.rel_2loop_frequency_df = pd.DataFrame.from_dict(self._dict_rel_2loop_frequency)
+        self.rel_2loop_frequency_df = self._fill_freq_matrix_with_nan(self.rel_2loop_frequency_df)
+
+    def normalize_relation_frequency_matrix(self):
+        if self.relation_frequency_df.empty:
+            print("Empty relation matrix")
+
+        relation_df = self.relation_frequency_df.copy(deep=True)
+        for i in relation_df.index:
+            for col in relation_df.columns:
+                if i == col:
+                    relation_df.loc[i, col] = self.calculate_self_loop_normalization(i, self.relation_frequency_df)
+                else:
+                    relation_df.loc[i, col] = self.calculate_causality_normalization(i, col, self.relation_frequency_df)
+
+        self.norm_relation_frequency_df = relation_df
         pass
 
-    def _set_direct_successors(self):
+    def normalize_2loop_frequency_matrix(self):
+        if self.rel_2loop_frequency_df.empty:
+            print("Empty two-loop matrix")
 
+        two_loop_df = self.rel_2loop_frequency_df.copy(deep=True)
+        for i in two_loop_df.index:
+            for col in two_loop_df.columns:
+                two_loop_df.loc[i, col] = self.calculate_two_loop_normalization(i, col, self.rel_2loop_frequency_df)
+
+        self.norm_rel_2loop_frequency_df = two_loop_df
+
+    @staticmethod
+    def calculate_causality_normalization(self, t1: str, t2: str, relation_df: pd.DataFrame):
+        """T1->T2"""
+        if np.isnan(relation_df.loc[t1, t2]):
+            return np.nan
+        else:
+            reverse_count = 0 if np.isnan(relation_df.loc[t2, t1]) else relation_df.loc[t2, t1]
+            return (relation_df.loc[t1, t2] - reverse_count)/(relation_df.loc[t1, t2] + reverse_count+1)
+
+    @staticmethod
+    def calculate_self_loop_normalization(self, t1: str, relation_df: pd.DataFrame):
+        """T1->T1"""
+        if np.isnan(relation_df.loc[t1, t1]):
+            return np.nan
+        else:
+            return relation_df.loc[t1, t1]/(relation_df.loc[t1, t1]+1)
+
+    @staticmethod
+    def calculate_two_loop_normalization(self, t1: str, t2: str, relation_df: pd.DataFrame):
+        """T1->^2 T2"""
+        if np.isnan(relation_df.loc[t1, t2]):
+            return np.nan
+        else:
+            reverse_loop_count = 0 if np.isnan(relation_df.loc[t2, t1]) else relation_df.loc[t2, t1]
+            return (relation_df.loc[t1, t2] + reverse_loop_count)/(relation_df.loc[t1, t2] + reverse_loop_count+1)
+
+    def _set_direct_successors(self):
         self._direct_successor_pairs = []
         for case in self.log.log:
             prev_task_name = None
@@ -41,23 +146,19 @@ class HeuristicMiner(BaseMiner):
                     self._direct_successor_pairs.append((prev_task_name, task['concept:name']))
                 prev_task_name = task['concept:name']
 
-    def set_relation_frequency_df(self):
-        if not self._direct_successor_pairs:
-            self._set_direct_successors()
-
-        for pair in set(self._direct_successor_pairs):
-            self._dict_relations_frequency[pair[0]][pair[1]] =self._direct_successor_pairs.count(pair)
-
-        self.relation_frequency_df = pd.DataFrame.from_dict(self._dict_relations_frequency).T
-        self.relation_frequency_df = self._fill_freq_matrix_with_nan(self.relation_frequency_df)
-
-
     def _fill_freq_matrix_with_nan(self, relation_df):
         missing_columns = set(self.TL).difference(set(relation_df.columns))
+        missing_rows = set(self.TL).difference(set(relation_df.index))
 
         for m_col in missing_columns:
             relation_df[m_col] = [np.NaN] * len(relation_df)
 
+        relation_df = relation_df.T
+        for m_row in missing_rows:
+            relation_df[m_row] = [np.NaN] * len(relation_df)
+        relation_df = relation_df.T
+
+        relation_df = relation_df.reindex(sorted(relation_df.index), axis=0)
         return relation_df.reindex(sorted(relation_df.columns), axis=1)
 
 
--- BP_mining/main_heuristic.py
+++ BP_mining/main_heuristic.py
@@ -12,18 +12,19 @@ if __name__ == "__main__":
     log_wrapped.to_list()
 
     heuristic = HeuristicMiner(log_file_path)
-    # heuristic.execute_algorithm()
+    heuristic.execute_algorithm()
 
     # filename = "./../results/heuristic_result"
     # heuristic.save_to_png(filename)
     #
     # print(heuristic.get_relation_df())
 
+    # heuristic.set_relation_frequency_matrix()
+    # heuristic.set_2loop_frequency_matrix()
 
+    # print(f"heuristic._direct_successor_pairs= {heuristic._direct_successor_pairs}")
 
-    heuristic.set_relation_frequency_df()
+    print(f"heuristic.relation_frequency_df=\n\n{heuristic.get_relation_frequency_matrix(normalized=False)}")
+    print("\n\n")
 
-    print(f"heuristic._direct_successor_pairs= {heuristic._direct_successor_pairs}")
-
-    print(f"heuristic.relation_frequency_df=\n\n{heuristic.relation_frequency_df}")
-
+    print(f"heuristic.rel_2loop_frequency_df=\n\n{heuristic.get_2loop_frequency_matrix(normalized=False)}")
