13acc27d556ce5f7f3ea96687f303ecf61f5fa99
Dorota Dydo-Rożniecka <34707786+DorotaDR@users.noreply.github.com>
2020-04-25 01:20:17 +0200

    Update gui_v2.py

--- BP_mining/gui_v2.py
+++ BP_mining/gui_v2.py
@@ -17,7 +17,6 @@ class Ui_MainWindow(object):
         MainWindow.resize(1200, 650)
         MainWindow.setWindowTitle("BPMN")
 
-
         self.centralwidget = QtWidgets.QWidget(MainWindow)
 
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.centralwidget)
@@ -31,45 +30,61 @@ class Ui_MainWindow(object):
         self.comboBox_alg = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox_alg.addItem("")
         self.comboBox_alg.addItem("")
-        self.comboBox_alg.setMaximumSize(QtCore.QSize(100, 16777215))
+        self.comboBox_alg.setMaximumSize(QtCore.QSize(100, 20))
 
 
         self.gridLayout.addWidget(self.comboBox_alg, 3, 0, 1, 1)
         self.label_param = QtWidgets.QLabel(self.centralwidget)
+        self.label_param.setMaximumSize(QtCore.QSize(16777215, 20))
 
         self.gridLayout.addWidget(self.label_param, 4, 0, 1, 1)
         self.doubleSpinBox_paramerer = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.doubleSpinBox_paramerer.setDecimals(2)
         self.doubleSpinBox_paramerer.setMaximum(1.0)
         self.doubleSpinBox_paramerer.setSingleStep(0.1)
-        self.doubleSpinBox_paramerer.setMaximumSize(QtCore.QSize(50, 16777215))
-
-
+        self.doubleSpinBox_paramerer.setMaximumSize(QtCore.QSize(50, 20))
         self.gridLayout.addWidget(self.doubleSpinBox_paramerer, 4, 1, 1, 1)
-        self.label_log_file_2 = QtWidgets.QLabel(self.centralwidget)
 
-        self.gridLayout.addWidget(self.label_log_file_2, 5, 0, 1, 1)
         self.label_log_file = QtWidgets.QLabel(self.centralwidget)
-
+        self.label_log_file.setMaximumSize(QtCore.QSize(16777215, 20))
         self.gridLayout.addWidget(self.label_log_file, 0, 0, 1, 1)
+
+
         self.lineEdit_filename = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit_filename.setReadOnly(True)
-        self.lineEdit_filename.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.lineEdit_filename.setMaximumSize(QtCore.QSize(200, 20))
 
 
         self.gridLayout.addWidget(self.lineEdit_filename, 1, 0, 1, 1)
         self.pushButton_choose_file = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_choose_file.clicked.connect(self.file_open)
-        self.pushButton_choose_file.setMaximumSize(QtCore.QSize(50, 16777215))
+        self.pushButton_choose_file.setMaximumSize(QtCore.QSize(50, 20))
 
 
         self.gridLayout.addWidget(self.pushButton_choose_file, 1, 1, 1, 1)
         self.label_alg = QtWidgets.QLabel(self.centralwidget)
+        self.label_alg.setMaximumSize(QtCore.QSize(16777215, 20))
 
         self.gridLayout.addWidget(self.label_alg, 2, 0, 1, 1)
+
+        self.pushButton_go = QtWidgets.QPushButton(self.centralwidget)
+        self.gridLayout.addWidget(self.pushButton_go, 5, 0, 1, 1)
+        self.pushButton_go.setText("Go!")
+        self.pushButton_go.setMaximumSize(QtCore.QSize(16777215, 20))
+
         self.verticalLayout_2.addLayout(self.gridLayout)
+
+        self.label_output = QtWidgets.QLabel(self.centralwidget)
+        self.label_output.setMaximumSize(QtCore.QSize(16777215, 15))
+        self.verticalLayout_2.addWidget(self.label_output)
+
+        self.comboBox_matrix = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox_matrix.addItem("Relation Matrix")
+        self.comboBox_matrix.addItem("2-loop Matrix")
+        self.verticalLayout_2.addWidget(self.comboBox_matrix)
+
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setMaximumSize(QtCore.QSize(300, 16777215))
+        self.textBrowser.setMaximumSize(QtCore.QSize(300, 300))
 
 
         self.verticalLayout_2.addWidget(self.textBrowser)
@@ -77,8 +92,10 @@ class Ui_MainWindow(object):
 
         self.scene = QtWidgets.QGraphicsScene()
         self.graphicsView = QtWidgets.QGraphicsView(self.scene, self.centralwidget)
-
         self.horizontalLayout.addWidget(self.graphicsView)
+
+        self.horizontalLayout.setStretch(0, 2)
+        self.horizontalLayout.setStretch(1, 3)
         self.horizontalLayout_2.addLayout(self.horizontalLayout)
 
         MainWindow.setCentralWidget(self.centralwidget)
@@ -102,7 +119,7 @@ class Ui_MainWindow(object):
         self.comboBox_alg.setItemText(0, _translate("MainWindow", "Alpha Miner"))
         self.comboBox_alg.setItemText(1, _translate("MainWindow", "Heuristic Miner"))
         self.label_param.setText(_translate("MainWindow", "ParameterValue"))
-        self.label_log_file_2.setText(_translate("MainWindow", "Output"))
+        self.label_output.setText(_translate("MainWindow", "Output"))
         self.label_log_file.setText(_translate("MainWindow", "Log file:"))
         self.pushButton_choose_file.setText(_translate("MainWindow", "Choose"))
         self.label_alg.setText(_translate("MainWindow", "Algorithm"))
@@ -128,7 +145,7 @@ class Ui_MainWindow(object):
 
         d = OrderedDict([('a', {}), ('b', {'a': 6, 'b': 3}), ('c', {'b': 3, 'd': 3}), ('d', {'c': 3, 'b': 3}), ('e', {'d': 2}), ('f', {'g': 3, 'd': 1, 'c': 3}), ('g', {'f': 3}), ('h', {'e': 2, 'f': 4})])
         df = pd.DataFrame.from_dict(d).fillna(" ")
-        # df.columns = [f"_{col}_" for col in df.columns]
+        df.columns = [f"_{col}_" for col in df.columns]
 
         self.textBrowser.setHtml(df.to_html())
 
